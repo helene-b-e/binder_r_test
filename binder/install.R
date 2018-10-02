6be5cad32d57ed9271b1b61e1a156a93058dfa1f
helene-b-e <helenebe@met.no>
2018-10-02 15:30:20 +0200

    testing binder

---
+++ binder/install.R
@@ -0,0 +1,10 @@
+# testing
+# 
+install.packages("netcdf4")
+install.packages("zoo")
+#install.packages("devtools")
+#extra
+install.packages("RgoogleMaps")
+install.packages("pryr")
+
+devtools::install_github("install.packages("metno/esd")
