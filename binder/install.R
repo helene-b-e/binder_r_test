0a9d2f33e017cd84eb9f8d5b9dd1291fc2c70b80
helene-b-e <helenebe@met.no>
2018-10-02 15:51:21 +0200

    fix

--- binder/install.R
+++ binder/install.R
@@ -1,10 +1,8 @@
 # testing
 # 
-install.packages("netcdf4")
+install.packages("ncdf4")
 install.packages("zoo")
 #install.packages("devtools")
-#extra
-install.packages("RgoogleMaps")
-install.packages("pryr")
+
 
 devtools::install_github("install.packages("metno/esd")
