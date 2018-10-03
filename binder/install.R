e25a1fda14a7ff0495db694208d778e694247e81
helene-b-e <helenebe@met.no>
2018-10-03 12:23:54 +0200

    jupytexttest

--- binder/install.R
+++ binder/install.R
@@ -3,6 +3,8 @@
 install.packages("ncdf4")
 install.packages("zoo")
 install.packages("devtools")
+install.packages("knitr")
+install.packages("RgoogleMaps")
 
 
 devtools::install_github("metno/esd")
